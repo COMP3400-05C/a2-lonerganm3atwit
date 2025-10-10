d760bbff7d41fbffc89555b962138c126af4a791
lonerganm <lonerganm3@wit.edu>
2025-10-10 14:36:20 -0400

    Completed array_create

--- array_create.c
+++ array_create.c
@@ -1,9 +1,27 @@
 #include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
+#include <stdlib.h>
 
 /**
  * TODO: Describe what the function does
  */
 int* array_create_evens(int begin, int end) {
-    return NULL;
+    if(begin >= end){
+        return NULL;
+    }
+    int num_nums = (end-begin+1)/2;
+    if(end%2 == 0 && begin%2 == 0){
+        num_nums++;
+    }
+    int arr_size = sizeof(int);
+    int *arr = malloc(num_nums*arr_size);
+
+    int n = begin + begin%2;
+    int i = 0;
+    while(i < num_nums){
+        arr[i] = n;
+        i++;
+        n+=2;
+    }
+
+    return arr;
 }
