5f440ba1dc0bfecbb3df15949d5ce13fc5af8dfe
lonerganm <lonerganm3@wit.edu>
2025-10-10 13:14:24 -0400

    Completed array_fill

--- array_fill.c
+++ array_fill.c
@@ -1,8 +1,22 @@
 #include <stddef.h> // For NULL
+#include <stdio.h>
 
 /**
- * TODO: Describe what the function does
+ * The function creates a variable to track how many numbers have been added to the array and determines the distance between the start and end numbers
+ * After that the array becomes populated with numbers until either the end number is reached or the array is full
+ * The function ends by returning the number of numbers added to the array
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    int i;
+    int distance = end - begin;
+    for(i = 0; i < array_len && i <= distance; i++){
+        array[i] = begin++;
+    }
+    /*printf("{%d", array[0]);
+    for(int j = 1; j < i; j++){
+        printf(", %d",array[j]);
+    }
+    printf("}\n");*/
+
+    return i;
 }
