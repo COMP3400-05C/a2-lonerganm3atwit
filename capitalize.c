d1a4d7b8ad913b67d43b91de8984a335d75e0961
lonerganm <lonerganm3@wit.edu>
2025-10-10 14:43:47 -0400

    Completed capitalize

--- capitalize.c
+++ capitalize.c
@@ -1,10 +1,29 @@
 #include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
+#include <stdlib.h>
+#include <ctype.h>
+
 
 /**
  * TODO: Describe what the function does
  */
 char* capitalize(const char* s) {
-    return NULL;
+    if(s == NULL){
+        return NULL;
+    }
+    int i = 0;
+    while(s[i] != '\0'){
+        i++;
+    }
+
+    int arr_size = sizeof(char);
+    char *capArr = malloc(++i*arr_size);
+    int j = 0;
+    while(s[j] != '\0'){
+        capArr[j] = toupper(s[j]);
+        j++;
+    }
+    capArr[j] = '\0';
+    
+    return capArr;
 }
 
